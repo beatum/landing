950f00ee8171787fbedc0eb550d81d5b0d90de23
beatum <direct@beatum-group.ru>
2015-05-20 00:39:17 +0300

    add files

---
+++ landing/page/views.py
@@ -0,0 +1,12 @@
+# from django.shortcuts import render
+
+from django.views.generic.base import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "page/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        return context
+
